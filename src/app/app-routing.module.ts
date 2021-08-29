22526d660ce7e46b83bdcfcf1d30aa5407183ea5
kevaf <kevinkipyegon20@gmail.com>
2021-08-29 19:08:36 +0300

    added home component to router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ReposComponent } from './repos/repos.component';
 import { UserDetsComponent } from './user-dets/user-dets.component';
 
 const routes: Routes = [
   {path:"findUser", component:UserDetsComponent},
   {path:"fetchRepo", component:ReposComponent},
-  {path:"", redirectTo:"findUser", pathMatch:""},
+  {path:"home", component:HomeComponent},
  
 ];
 
