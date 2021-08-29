0042186f204857551cfa0892dd70cf80e6c55c20
kevaf <kevinkipyegon20@gmail.com>
2021-08-29 11:43:18 +0300

    added router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReposComponent } from './repos/repos.component';
+import { UserDetsComponent } from './user-dets/user-dets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"FindUser", component:UserDetsComponent},
+  {path:"FetchRepo", component:ReposComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
